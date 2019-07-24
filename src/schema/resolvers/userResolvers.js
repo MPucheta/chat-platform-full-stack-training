04b8ac09dbde73bf0081fb20df1f5c7bb560cac6
ShephardMP <smpucheta@gmail.com>
2019-07-24 06:15:58 -0300

    MP/3. Added graphql implementation with sequelize integration

--- src/schema/resolvers/userResolvers.js
+++ src/schema/resolvers/userResolvers.js
@@ -1,88 +1,21 @@
-class User {
-  constructor (id, firstName, lastName, username, password, salt) {
-    this._id = id
-    this._firstName = firstName
-    this._lastName = lastName
-    this._username = username
-    this._password = password
-    this._salt = salt
-  }
+import db from '../../../models/index'
+import UserSequelizeModel from '../../../models/user'
 
-  // getters/setters if we ever need to enforce some rules
-  // for example, not allowing the salt to change
-  get id () {
-    return this._id
-  }
-
-  set id (id) {
-    this._id = id
-  }
-
-  get firstName () {
-    return this._firstName
-  }
-
-  set firstName (firstName) {
-    this._firstName = firstName
-  }
-
-  get lastName () {
-    return this._lastName
-  }
-
-  set lastName (lastName) {
-    this._lastName = lastName
-  }
-
-  get username () {
-    return this._username
-  }
-
-  set username (username) {
-    this._username = username
-  }
-
-  get password () {
-    return this._password
-  }
-
-  set password (password) {
-    this._password = password
-  }
-
-  get salt () {
-    return this._salt
-  }
-
-  set salt (salt) {
-    this._salt = salt
-  }
-}
-
-const userFakeDatabase = []
-userFakeDatabase.push(new User(0, 'firstName0', 'lastname0', 'username0', 'password0', 'saltShouldNotBeShownFor0'))
-userFakeDatabase.push(new User(1, 'firstName1', 'lastname1', 'username1', 'password1', 'saltShouldNotBeShownFor1'))
+const uuid = require('uuid/v4')
+const UserModel = UserSequelizeModel(db.sequelize, db.Sequelize.DataTypes)
 
 const resolvers = {
   Query: {
 
     users: () => {
-      /*
-      Placeholder for sequelize implementation:
-      User.findAll()
-      .then( (users) => return users)
-      */
-      return userFakeDatabase
+      return UserModel.findAll()
+        .then((users) => { return users })
     },
 
-    user: (_, { id }) => {
-      /*
-      Placeholder for sequelize implementation:
-      User.findOne( { where: { id : id}})
-      .then( (user) => return user)
-      .catch(`an error occurred when fetching user ${id}`)
-      */
-      return userFakeDatabase.find((user) => user.id === id)
+    user: (_, { username }) => {
+      return UserModel.findOne({ where: { username: username } })
+        .then((user) => { return user })
+        .catch(`an error occurred when fetching user ${username}`)
     }
 
   },
@@ -111,20 +44,16 @@ const resolvers = {
   },
 
   Mutation: {
-    createUser: (_, { id, firstName, lastName, username, password, salt }) => {
-      const newUser = new User(id, firstName, lastName, username, password, salt)
-      userFakeDatabase.push(newUser)
-      return newUser
-
-      /*
-         PlaceHolder for sequelize implementation:
-      const newUser
-      User.create({ id: id, firstName: firstName, lastName: lastName, username: username, password: password, salt: salt })
-      .then( () => User.findOrCreate( { where: { username: username} })
-      .then( ([user, created]) => newUser = user))
-      .then( return newUser ) //I am not sure if putting this return in the function's body will work as I think newUser would be undefined before the execution of the Promise
-      .catch( console.log(`An error occurred when creating a new user`))
-      */
+    createUser: (_, { firstName, lastName, username, password }) => {
+      const newUUID = uuid()
+      return UserModel.create({ id: newUUID,
+        firstName: firstName,
+        lastName: lastName,
+        username: username,
+        password: password
+      }).then(() => UserModel.findOrCreate({ where: { username: username } })
+        .then(([user, created]) => { return user }))
+        .catch(console.log(`An error occurred when creating a new user`))
     }
   }
 }
