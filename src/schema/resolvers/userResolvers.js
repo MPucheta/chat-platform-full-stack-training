323f441854d7e13ed4dbe3defa3dd23b78443ae3
ShephardMP <smpucheta@gmail.com>
2019-07-25 17:25:11 -0300

    Solved Abel's suggestions (UUIDs, 'then' usage, refactored syntax, root imports and user.graphl queries/mutations

--- src/schema/resolvers/userResolvers.js
+++ src/schema/resolvers/userResolvers.js
@@ -1,59 +1,19 @@
-import db from '../../../models/index'
-import UserSequelizeModel from '../../../models/user'
-
-const uuid = require('uuid/v4')
-const UserModel = UserSequelizeModel(db.sequelize, db.Sequelize.DataTypes)
+import models from '~/models'
 
 const resolvers = {
   Query: {
 
-    users: () => {
-      return UserModel.findAll()
-        .then((users) => { return users })
-    },
+    users: () => models.user.findAll(),
 
-    user: (_, { username }) => {
-      return UserModel.findOne({ where: { username: username } })
-        .then((user) => { return user })
-        .catch(`an error occurred when fetching user ${username}`)
-    }
-
-  },
-
-  User: {
-
-    id: (user) => {
-      return user.id
-    },
-    firstName: (user) => {
-      return user.firstName
-    },
-    lastName: (user) => {
-      return user.lastName
-    },
-    username: (user) => {
-      return user.username
-    },
-    password: (user) => {
-      return user.password
-    }
+    // when using only one parameter you can skip the usage of { }
+    user: (_, username) => models.user.findOne({ where: username })
 
   },
 
   Mutation: {
-    createUser: (_, { firstName, lastName, username, password }) => {
-      const newUUID = uuid()
 
-      return UserModel.create({
-        id: newUUID,
-        firstName: firstName,
-        lastName: lastName,
-        username: username,
-        password: password
-      }).then(() => UserModel.findOrCreate({ where: { username: username } })
-        .then(([user, created]) => { return user }))
-        .catch(console.log(`An error occurred when creating a new user`))
-    }
+    createUser: (_, userData) => models.user.create(userData)
+
   }
 }
 
