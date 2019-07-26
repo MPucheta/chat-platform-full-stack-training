aa366bd6814c98adffccfdf0e509b64119c14963
ShephardMP <smpucheta@gmail.com>
2019-07-25 23:29:34 -0300

    Also fixed blanks

--- src/schema/resolvers/userResolvers.js
+++ src/schema/resolvers/userResolvers.js
@@ -2,18 +2,13 @@ import models from '~/src/models'
 
 const resolvers = {
   Query: {
-
     users: () => models.user.findAll(),
-
     // when using only one parameter you can skip the usage of { }
     user: (_, username) => models.user.findOne({ where: username })
-
   },
 
   Mutation: {
-
     createUser: (_, userData) => models.user.create(userData)
-
   }
 }
 
