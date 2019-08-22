71f1a1e396055f09b68add842e477613f76af769
ShephardMP <smpucheta@gmail.com>
2019-08-22 17:49:18 -0300

    [REFACTORED] - changed jwt to have a default value for expiresIn

--- src/jwt/index.js
+++ src/jwt/index.js
@@ -1,8 +1,8 @@
 import jsonwebtoken from 'jsonwebtoken'
 
-const expiresIn = process.env.JWT_LIFESPAN || '10d'
+const DEFAULT_JWT_LIFESPAN = process.env.JWT_LIFESPAN || '10d'
 
-function generateJWT (payload, subject) {
+function generateJWT (payload, subject, expiresIn = DEFAULT_JWT_LIFESPAN) {
   return jsonwebtoken.sign(payload, process.env.SECRET, { subject, expiresIn })
 }
 
