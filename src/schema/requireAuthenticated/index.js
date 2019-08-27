3b36ee58a8c49721e97736f2cf85a02ae2e1cb18
ShephardMP <smpucheta@gmail.com>
2019-08-27 13:15:08 -0300

    [STYLE] added parenthesis to multiline statement in lambda

--- src/schema/requireAuthenticated/index.js
+++ src/schema/requireAuthenticated/index.js
@@ -1,8 +1,9 @@
 import { skip } from 'graphql-resolvers'
 
-const requireAuthenticated = (root, args, context, info) =>
+const requireAuthenticated = (root, args, context, info) => (
   context.currentUser
     ? skip
     : new Error('Not authenticated')
+)
 
 export default requireAuthenticated
